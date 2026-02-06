7b0a95e0f9bd4d7d1c3fba012c7aa5bac252b712
STUDENT <kate_pyankova@mail.ru>
2026-02-06 10:58:05 +0500

    Initial commit

---
+++ mylibrary/src/main/java/com/example/mylibrary/Bluetooth.kt
@@ -0,0 +1,15 @@
+package com.example.mylibrary
+
+import android.content.Context
+import android.widget.Toast
+
+class Bluetooth{
+
+
+    fun Password (context: Context){
+        Toast.makeText(context, "OK", Toast.LENGTH_SHORT).show()
+    }
+    fun Login (context: Context){
+        Toast.makeText(context, "lkj;lk", Toast.LENGTH_SHORT).show()
+    }
+}
